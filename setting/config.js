d926ddf071bbf80eaba876cc11542037628c3584
sharz7 <nwazir.official22@gmail.com>
2026-08-19 13:29:30 +0500

    Update config.js

--- setting/config.js
+++ setting/config.js
@@ -1,53 +1,53 @@
 const fs = require('fs')
 
-global.owner = "923271054080" //owner number
-global.footer = "༒︎ 𝑺𝑯𝑨𝑫𝑶𝑾 ༒︎" //footer section
+global.owner = "923238409187" //owner number
+global.footer = "SHARZ" //footer section
 global.status = false //"self/public" section of the bot
 global.prefa = ['','!','.','#','&','/','@']
 global.owner = ['234']
 global.xprefix = '.'
 global.gambar = "https://i.postimg.cc/NMn8rzqh/image1.png"
-global.OWNER_NAME = "@shadowhacr" //
+global.OWNER_NAME = "@sharz7" //
 global.DEVELOPER = ["7848300179"] //
-global.BOT_NAME = "☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎︎"
-global.bankowner = "☠︎︎ 𓆩𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍𓆪 ☠︎︎"
-global.creatorName = "☠︎︎ 𓆩𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍𓆪 ☠︎︎"
-global.ownernumber = '923271054080'  //creator number
-global.location = "Pakistan, Punjab, sangla hill"
+global.BOT_NAME = "SHARZ MD"
+global.bankowner = "SHARZ Official"
+global.creatorName = "SHARZ Official"
+global.ownernumber = '923238409187'  //creator number
+global.location = "Pakistan, kpk, D i khan"
 global.prefa = ['','!','.','#','&']
 //================DO NOT CHANGE OR YOU'LL GET AN ERROR=============\
 global.gcmess = "☠️ 𖤐 𝐀𝐂𝐂𝐄𝐒𝐒 𝐃𝐄𝐍𝐈𝐄𝐃 𖤐 ☠️  𖥂 𝐓𝐡𝐢𝐬 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐜𝐚𝐧 𝐨𝐧𝐥𝐲 𝐛𝐞 𝐬𝐮𝐦𝐦𝐨𝐧𝐞𝐝 𝐢𝐧 𝐭𝐡𝐞 𝐝𝐞𝐩𝐭𝐡𝐬 𝐨𝐟 𝐠𝐫𝐨𝐮𝐩𝐬.  🕳️ 𝐓𝐫𝐲𝐢𝐧𝐠 𝐢𝐭 𝐞𝐥𝐬𝐞𝐰𝐡𝐞𝐫𝐞 𝐰𝐢𝐥𝐥 𝐛𝐞 𝐢𝐠𝐧𝐨𝐫𝐞𝐝… 𝐨𝐫 𝐰𝐨𝐫𝐬𝐞."
 global.adminmess = "☠️ 𖤐 𝐀𝐃𝐌𝐈𝐍 𝐑𝐄𝐒𝐓𝐑𝐈𝐂𝐓𝐈𝐎𝐍 𖤐 ☠️  𖥂 𝐓𝐡𝐢𝐬 𝐟𝐞𝐚𝐭𝐮𝐫𝐞 𝐢𝐬 𝐫𝐞𝐬𝐭𝐫𝐢𝐜𝐭𝐞𝐝 𝐭𝐨 𝐚𝐝𝐦𝐢𝐧𝐬 𝐨𝐧𝐥𝐲.  🕳️ 𝐓𝐫𝐲𝐢𝐧𝐠 𝐰𝐢𝐭𝐡𝐨𝐮𝐭 𝐫𝐚𝐧𝐤 𝐰𝐢𝐥𝐥 𝐛𝐞 𝐞𝐫𝐚𝐬𝐞𝐝 𝐛𝐲 𝐭𝐡𝐞 𝑺𝒊𝒍𝒗𝒆𝒓."
 global.premmess = "☠️ 𖤐 𝐑𝐄𝐒𝐓𝐑𝐈𝐂𝐓𝐄𝐃 𝐅𝐄𝐀𝐓𝐔𝐑𝐄 𖤐 ☠️  𖥂 𝐓𝐡𝐢𝐬 𝐟𝐞𝐚𝐭𝐮𝐫𝐞 𝐢𝐬 𝐫𝐞𝐬𝐞𝐫𝐯𝐞𝐝 𝐟𝐨𝐫 𝐩𝐫𝐞𝐦𝐢𝐮𝐦 𝐮𝐬𝐞𝐫𝐬 𝐨𝐧𝐥𝐲.  🕳️ 𝐔𝐩𝐠𝐫𝐚𝐝𝐞 𝐲𝐨𝐮𝐫 𝐫𝐚𝐧𝐤… 𝐨𝐫 𝐟𝐚𝐥𝐥 𝐢𝐧𝐭𝐨 𝐨𝐛𝐥𝐢𝐯𝐢𝐨𝐧."
 global.dev = "923271054080"
-global.creatorName = "☠︎︎ 𓆩𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍𓆪 ☠︎︎'"
-global.footer = "☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎" //footer section
+global.creatorName = "SHARZ Official'"
+global.footer = "SHARZ MD" //footer section
 global.link = "https://whatsapp.com/channel/0029Vb6iopUDzgTJuzPCk32V"
 global.autobio = true//auto update bio
-global.botName = "☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎"
+global.botName = "SHARZ MD"
 global.version = "V2"
-global.botname = "☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎"
-global.author = "༒︎ 𝑺𝑯𝑨𝑫𝑶𝑾 ༒︎"
+global.botname = "SHARZ MD"
+global.author = "SHARZ"
 global.themeemoji = '🤞'
 global.wagc = 'https://whatsapp.com/channel/0029Vb6iopUDzgTJuzPCk32V'
 global.thumbnail = 'https://i.postimg.cc/qvrFRzxG/thumb.png'
 global.richpp = 'https://i.postimg.cc/jjdkHm9n/scar1.png'
 global.packname = "𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒 𖣘♔︎"
-global.author = "༒︎ 𝑺𝑯𝑨𝑫𝑶𝑾 ༒︎"
-global.creator = "923271054080@s.whatsapp.net"
-global.ownername = '☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍 ☠︎︎' 
+global.author = "SHARZ"
+global.creator = "923238409187@s.whatsapp.net"
+global.ownername = 'SHARZ Official' 
 global.onlyowner = `☠️ 𝙎𝙤𝙧𝙧𝙮, 𝙏𝙝𝙞𝙨 𝙁𝙚𝙖𝙩𝙪𝙧𝙚 𝙄𝙨 𝙍𝙚𝙨𝙩𝙧𝙞𝙘𝙩𝙚𝙙 𝙏𝙤 𝙊𝙬𝙣𝙚𝙧𝙨 𝙊𝙣𝙡𝙮.  
 🩸 𝘾𝙤𝙣𝙩𝙖𝙘𝙩 *SHADOW* 𝙩𝙤 𝙜𝙖𝙞𝙣 𝙤𝙬𝙣𝙚𝙧 𝙥𝙧𝙞𝙫𝙞𝙡𝙚𝙜𝙚𝙨.  
-🕳️ 𝙄𝙡𝙡𝙚𝙜𝙖𝙡 𝙖𝙘𝙘𝙚𝙨𝙨 𝙬𝙞𝙡𝙡 𝙗𝙚 𝙘𝙡𝙚𝙖𝙣𝙨𝙚𝙙 𝙗𝙮 𝙩𝙝𝙚 𝑻𝒂𝒍𝒉𝒂.`
+🕳️ 𝙄𝙡𝙡𝙚𝙜𝙖𝙡 𝙖𝙘𝙘𝙚𝙨𝙨 𝙬𝙞𝙡𝙡 𝙗𝙚 𝙘𝙡𝙚𝙖𝙣𝙨𝙚𝙙 𝙗𝙮 𝙩𝙝𝙚 SHARZ.`
   // reply 
 global.database = `☠️ 𝙏𝙤 𝙗𝙚 𝙖𝙙𝙙𝙚𝙙 𝙩𝙤 𝙩𝙝𝙚 𝘿𝘼𝙏𝘼𝘽𝘼𝙎𝙀 𝘽𝘼𝙎𝙀...  
-🩸 𝘾𝙤𝙣𝙩𝙖𝙘𝙩 *SHADOW* — 𝙊𝙧 𝙗𝙚 𝙚𝙧𝙖𝙨𝙚𝙙 𝙗𝙮 𝙙𝙚𝙛𝙖𝙪𝙡𝙩.  🔗 *No entry without approval.*`
+🩸 𝘾𝙤𝙣𝙩𝙖𝙘𝙩 *SHARZ* — 𝙊𝙧 𝙗𝙚 𝙚𝙧𝙖𝙨𝙚𝙙 𝙗𝙮 𝙙𝙚𝙛𝙖𝙪𝙡𝙩.  🔗 *No entry without approval.*`
   global.mess = {
-wait: "```☠️ WAIT FOR  ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎😈 ⚡ *Darkness intensifies...*  💀 *Brace yourself — The SHADOW is rising.*```",
-   success: "✔️ 𝗦𝗨𝗖𝗖𝗘𝗦𝗦 𝗕𝗬 ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎ ⚔️`  🔥 *Mission complete. The Void accepts your command.*",
-   on: "`✔️☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎𝗔𝗖𝗧𝗜𝗩𝗘 ⚔️`  🔥 *The SHADOW is unleashed. Power running at full force.*  🕳️ *Enter if you dare.*", 
+wait: "```☠️ WAIT FOR SHARZ MD*Darkness intensifies...*  💀 *Brace yourself — The SHADOW is rising.*```",
+   success: "✔️ 𝗦𝗨𝗖𝗖𝗘𝗦𝗦 𝗕𝗬 SHARZ MD`  🔥 *Mission complete. The Void accepts your command.*",
+   on: "`✔️SHARZ 𝗔𝗖𝗧𝗜𝗩𝗘 ⚔️`  🔥 *The SHADOW is unleashed. Power running at full force.*  🕳️ *Enter if you dare.*", 
    prem: "`⛧ PREMIUM ACCESS ONLY ⛧`  🩸 *This feature is restricted to premium users.*  📲 *Add your number to the database to unlock the dark tier.*  🕳️ *No entry without clearance. The SHADOW does not negotiate.*", 
-   off: "`❌ ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗢𝗙𝗙 ⚰️`  🕳️ *The SHADOW has gone dark. All systems offline.*  ⚡ *Recharge and prepare for the next awakening.*",
+   off: "`❌ SHARZ 𝗣𝗢𝗪𝗘𝗥𝗘𝗗 𝗢𝗙𝗙 ⚰️`  🕳️ *The SHARZ has gone dark. All systems offline.*  ⚡ *Recharge and prepare for the next awakening.*",
    query: {
        text: "Where's the text, man?",
        link: "Where's the link, bro?",
@@ -65,14 +65,14 @@ private: "Sorry bro, This Feature Can Only Be Used In Private Chats",
    }
 }
 
-global.onlyowner = `\`[ 🤞 ] ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎ \` \n*
+global.onlyowner = `\`[ 🤞 ] SHARZ \` \n*
 
          🚫 𝔸ℂℂ𝔼𝕊𝕊 𝔻𝔼ℕ𝕀𝔼𝔻 🚫
 
 𝕊𝕆ℝℝ𝕐... 𝕋ℍ𝕀𝕊 𝕔𝕠𝕞𝕞𝕒𝕟𝕕 𝕓𝕝𝕠𝕠𝕕𝕤𝕙𝕖𝕕𝕤 𝕗𝕠𝕣 𝕐𝕆𝕌
 𝕆ℕ𝕃𝕐 𝔹𝕃𝔸𝔻𝔼𝔻 𝕌𝕊𝔼ℝ𝕊 𝕀ℕ 𝕆𝕌ℝ 𝔻𝔸𝕋𝔸𝔹𝔸𝕊𝔼 𝕄𝔸𝕐 𝔼𝕟𝕥𝕖𝕣 ⚔️ `
   
-global.database = `\`[ 👑 ] ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎ 😙 \` \n*
+global.database = `\`[ 👑 ] SHARZ MD \` \n*
 
         🚫 𝔸ℂℂ𝔼𝕊𝕊 𝔻𝔼ℕ𝕀𝔼𝔻 🚫
 
@@ -82,11 +82,11 @@ global.database = `\`[ 👑 ] ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎
 
 𝕮𝖔𝖓𝖙𝖆𝖈𝖙 𝕺𝖚𝖗 𝕺𝖋𝖋𝖎𝖈𝖎𝖆𝖑𝖘 𝕭𝖊𝖋𝖔𝖗𝖊 𝖞𝖔𝖚 𝖋𝖆𝖑𝖑:
 
-🔥 𝖂𝖆: wa.me/923271054080
-🕷️ 𝖙𝖌: t.me/shadowhacr
+🔥 𝖂𝖆: wa.me/923238409187
+🕷️ 𝖙𝖌: t.me/sharzwazir3
 💀 𝖜𝖆 𝖈𝖍𝖆𝖓𝖓𝖊𝖑: https://whatsapp.com/channel/0029Vb6iopUDzgTJuzPCk32V
 
-𝕋𝕙𝕒𝕟𝕜𝕤 𝕗𝕠𝕣 𝕌𝕤𝕚𝕟𝕘  ☠︎︎ 𝑺𝒉𝒂𝒅𝒐𝒘 𝑴𝑫 ☠︎︎`
+𝕋𝕙𝕒𝕟𝕜𝕤 𝕗𝕠𝕣 𝕌𝕤𝕚𝕟𝕘  SHARZ MD `
 
 global.hituet = 0
 //false=disable and true=enable
